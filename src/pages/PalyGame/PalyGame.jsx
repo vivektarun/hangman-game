d0acbb0b391c288769e3c91c3c1a1fc8e4d79893
vivektarun <vivektarun1234@gmail.com>
2025-08-26 16:07:00 +0530

    feat: add buttons and masked text

--- src/pages/PalyGame/PalyGame.jsx
+++ src/pages/PalyGame/PalyGame.jsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
+import MaskedText from "../../components/MaskedText/MaskedText";
+import LetterButtons from "../../components/LetterButtons/LetterButtons";
 
 function PlayGame() {
 
     return (
         <div>
-            <h1>------------Play Game--------------</h1>
+            <h1>--------------Play Game---------------</h1>
+            
+            <MaskedText text="humble" usedLetters={['b', 'e']} />
+            <hr/>
 
-            <Link to='/start'>Go to Start Game </Link>
+            <LetterButtons usedLetters={['b', 'e']} />
+            <hr />
+
+            <Link to='/start'> Start Game </Link>
         </div>
     );
 }
